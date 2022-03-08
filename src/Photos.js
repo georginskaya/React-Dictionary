40046b4be0c295e3b7480fb58ef8b8cb367b1dfd
georginskaya <georginskaya@gmail.com>
2022-03-08 11:28:42 +0100

    alt added

--- src/Photos.js
+++ src/Photos.js
@@ -10,9 +10,9 @@ export default function Photos (props) {
 
 {props.photos.map(function(photo,index){
 return (
-    <div className="col-4" key={index}>
+    <div className="col-4" key={index} alt ="img">
         <a href={photo.src.original} target="_blank" rel="noreferrer">
-    <img src={photo.src.landscape} className="img-fluid" />
+    <img src={photo.src.landscape} alt ="img" className="img-fluid" />
 </a>
 </div> 
 );
